dbb98a989292a4501acb0df6b3aacbf10d9371cd
Artem Deynega <artemdeynega892@gmail.com>
2021-07-02 14:56:13 +0300

    3

--- src/js/02-feedback.js
+++ src/js/02-feedback.js
@@ -1,43 +1,55 @@
-import trottle from 'lodash.throttle';
-import '../css/feedback-form.css';
+import throttle from 'lodash.throttle';
 import '../css/common.css';
+import '../css/feedback-form.css';
 
 const STORAGE_KEY = 'feedback-msg';
 
 const refs = {
     form: document.querySelector('.js-feedback-form'),
-    textarea: document.querySelector('.js-feedback-form textarea'),
+    textarea: document.querySelector('.js-feedback-form  textarea'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener(
     'input',
-    trottle(onTextereaInput, 1000),
+    throttle(onTextareaInput, 200),
 );
 
 populateTextarea();
 
+/*
+ * - Останавливаем поведение по умолчанию
+ * - Убираем сообщение из хранилища
+ * - Очищаем форму
+ */
 function onFormSubmit(evt) {
     evt.preventDefault();
 
     console.log('Отправляем форму');
-
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
 
-function onTextereaInput(evt) {
+/*
+ * - Получаем значение поля
+ * - Сохраняем его в хранилище
+ * - Можно добавить throttle
+ */
+function onTextareaInput(evt) {
     const message = evt.target.value;
 
     localStorage.setItem(STORAGE_KEY, message);
 }
 
+/*
+ * - Получаем значение из хранилища
+ * - Если там что-то было, обновляем DOM
+ */
 function populateTextarea() {
     const savedMessage = localStorage.getItem(STORAGE_KEY);
 
     if (savedMessage) {
         refs.textarea.value = savedMessage;
-        console.log(savedMessage);
     }
 }
 
