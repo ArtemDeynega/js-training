7596cf26426652047b09741a767f1c0f6600dd73
Artem Deynega <artemdeynega892@gmail.com>
2021-07-07 15:38:08 +0300

    дата и время

--- src/events.js
+++ src/events.js
@@ -2,22 +2,36 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/brands';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import 'modern-normalize/modern-normalize.css';
-// import { format } from 'date-fns';
+import { format } from 'date-fns';
 // // import { uk } from 'date-fns/locale';
 import upcomingEvents from './upcoming-events.json';
 import eventsTemplate from './templates/events.hbs';
 import './sass/modules/events/index.scss';
 
-const eventBoard = document.querySelector('.js-event-board');
-console.log(eventBoard);
+const eventBoard = document.querySelector(
+    '.js-event-board',
+);
 
-eventBoard.innerHTML = eventsTemplate(upcomingEvents);
-
-// eventBoard.innerHTML = eventsTemplate({
-//   events: upcomingEvents.map(event => ({
-//     ...event,
-//     start: format(Date.parse(event.start), 'd MMMM yyyy, HH:mm', {
-//       // locale: uk,
+eventBoard.innerHTML = eventsTemplate(
+    upcomingEvents.map(evt => {
+        return {
+            ...evt,
+            start: format(
+                Date.parse(evt.start),
+                'dd MMMM yyyy, HH:mm',
+            ),
+        };
+    }),
+);
+// console.log(Date.parse('2022-02-17T14:30:00'));
+// console.log(
+//     upcomingEvents.map(evt => {
+//         return {
+//             ...evt,
+//             start: format(
+//                 Date.parse(evt.start),
+//                 'dd MMMM yyyy, HH:mm',
+//             ),
+//         };
 //     }),
-//   })),
-// });
+// );
