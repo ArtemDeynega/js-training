f359cba0b7f090e4ba700ac708f85387d486f650
Artem Deynega <artemdeynega892@gmail.com>
2021-07-14 17:16:02 +0300

    Интревалы

--- src/js/02-interval.js
+++ src/js/02-interval.js
@@ -4,20 +4,21 @@ import '../css/common.css';
  * Метод setInterval(callback, delay, args)
  */
 
-const logger = time => console.log(`Лог каждые ${time}ms - ${Date.now()}`);
+const logger = time =>
+    console.log(`Лог каждый ${time}ms - ${Date.now()}`);
 
 // console.log('До вызова setInterval');
-// setInterval(logger, 2000, 2000);
+// // setInterval(logger, 2000, 2000);
 // console.log('После вызова setInterval');
 
 /*
  * Очистка интервала с clearInterval(intervalId)
  */
 
-// const intervalId = setInterval(logger, 2000, 2000);
-// const shouldCancelInterval = Math.random() > 0.3;
-// console.log(shouldCancelInterval);
+const intervalId = setInterval(logger, 2000, 2000);
+const shouldCancelInterval = Math.random() > 0.3;
+console.log(shouldCancelInterval);
 
-// if (shouldCancelInterval) {
-//   clearInterval(intervalId);
-// }
+if (!shouldCancelInterval) {
+    clearInterval(intervalId);
+}
