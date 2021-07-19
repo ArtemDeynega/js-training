dbe75483b3aaed4edf944cd52fed44b413711fdb
Artem Deynega <artemdeynega892@gmail.com>
2021-07-19 18:17:09 +0300

    Промисификация и Покемоны

--- src/js/02-promisification.js
+++ src/js/02-promisification.js
@@ -59,42 +59,43 @@
  * Покемоны с https://pokeapi.co/
  */
 
-// const fetchPokemonById = id => {
-//   return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(r => r.json());
-// };
+// function fetchPokemonById(id) {
+//     return fetch(
+//         `https://pokeapi.co/api/v2/pokemon/${id}`,
+//     ).then(r => r.json());
+// }
 
-// fetchPokemonById(1).then(onFetchSuccess).catch(onFetchError);
+// fetchPokemonById(515)
+//     .then(onFetchSuccess)
+//     .catch(onFetchError);
 
-// fetchPokemonById(2).then(onFetchSuccess).catch(onFetchError);
-
-// fetchPokemonById(3).then(onFetchSuccess).catch(onFetchError);
+// fetchPokemonById(615)
+//     .then(onFetchSuccess)
+//     .catch(onFetchError);
+// fetchPokemonById(715)
+//     .then(onFetchSuccess)
+//     .catch(onFetchError);
 
 // function onFetchSuccess(pokemon) {
-//   console.log('onFetchSuccess -> onFetchSuccess');
-//   console.log(pokemon);
+//     console.log(pokemon);
 // }
-
 // function onFetchError(error) {
-//   console.log('onFetchError -> onFetchError');
-//   console.log('Это в блоке catch');
-//   console.log(error);
+//     console.log('Ето в Кетч');
+//     console.log(error);
 // }
 
-// makePromise
-// const makePromise = () => {
-//   return new Promise((resolve, reject) => {
-//     const passed = Math.random() > 0.5;
-
-//     setTimeout(() => {
-//       if (passed) {
-//         resolve('✅ Куку это resolve');
-//       }
-
-//       reject('❌ все пропало это reject');
-//     }, 2000);
-//   });
-// };
-
-// makePromise()
-//   .then(result => console.log(result))
-//   .catch(error => console.log(error));
+const makePromise = () => {
+    return new Promise((resolve, reject) => {
+        const DELAY = 2000;
+        setTimeout(() => {
+            const passed = Math.random() > 0.3;
+            if (passed) {
+                resolve('✅ куку это resolve');
+            }
+            reject('❌ произошла ошибка');
+        }, DELAY);
+    });
+};
+makePromise()
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
