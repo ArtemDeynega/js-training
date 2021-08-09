0c986dbd08e71176d2f4343a64c78feaa81c4946
Artem Deynega <artemdeynega892@gmail.com>
2021-08-09 15:12:15 +0300

    2

--- src/js/r - get.js
+++ src/js/r - get.js
@@ -1,17 +1,17 @@
-const BASE_URL = 'http://localhost:4040';
+const BASE_URL = 'http://localhost:3000/';
+const RESURS_BOOKS = 'books/';
 
 function fetchBooks() {
-    return fetch(`${BASE_URL}/books`).then(res =>
-        res.json(),
-    );
+    return fetch(`${BASE_URL}${RESURS_BOOKS}`)
+        .then(res => res.json())
+        .then(console.log);
 }
 
 function fetchBookById(bookId) {
-    return fetch(`${BASE_URL}/books/${bookId}`).then(res =>
-        res.json(),
-    );
+    return fetch(`${BASE_URL}${RESURS_BOOKS}${bookId}`)
+        .then(res => res.json())
+        .then(console.log);
 }
 
 fetchBooks();
-fetchBookById(2);
-fetchBookById(4);
+fetchBookById(3);
