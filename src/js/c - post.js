83821ca13b8a7af09a0b0b14f904fec385d5ce04
Artem Deynega <artemdeynega892@gmail.com>
2021-08-15 16:03:17 +0300

    23

--- src/js/c - post.js
+++ src/js/c - post.js
@@ -1,9 +1,42 @@
 const BASE_URL = 'http://localhost:3000/';
-const RESURSE_BOOKS = 'books/';
+const RESURSE_BOOKS = 'books';
 
-const newBook = {
+function addBook(book) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(book),
+    };
+
+    return fetch(
+        `${BASE_URL}${RESURSE_BOOKS}`,
+        options,
+    ).then(res => res.json());
+}
+
+addBook({
     title: 'Тестовая книга',
     author: 'Я',
     genres: ['приключения JS'],
     rating: 10,
-};
+}).then(console.log);
+addBook({
+    title: 'Тестовая книга по HTML',
+    author: 'Я',
+    genres: ['приключения HTML'],
+    rating: 3,
+}).then(console.log);
+addBook({
+    title: 'Тестовая книга по CSS',
+    author: 'Я',
+    genres: ['приключения CSS'],
+    rating: 5,
+}).then(console.log);
+addBook({
+    title: 'Тестовая книга по JS',
+    author: 'Я',
+    genres: ['приключения JS'],
+    rating: 8,
+}).then(console.log);
